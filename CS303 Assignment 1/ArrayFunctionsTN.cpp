58db1749693ebd98408736517d62b1be50d3249a
Trevor Norton <nortrevor02@gmail.com>
2024-09-03 10:09:30 -0500

    Began implementation

--- CS303 Assignment 1/ArrayFunctionsTN.cpp
+++ CS303 Assignment 1/ArrayFunctionsTN.cpp
@@ -1,7 +1,13 @@
 #include "ArrayFunctionsTN.h"
 
 int search(int input[], int target) {
-
+	int foundIndex = 0;
+	for (int i = 0; i < sizeof(input); i++) { //This checks if the input target is found in the array.
+		if (input[i] == target) {
+			foundIndex = i; //This sets the foundIndex to the index of the item if found.
+		}
+	}
+	return input[foundIndex];
 }
 
 void modify(int input[], int index) {
--- CS303 Assignment 1/CS303 Assignment 1.cpp
+++ CS303 Assignment 1/CS303 Assignment 1.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "ArrayFunctionsTN.h"
 using namespace std;
 
 
 int main()
 {
+    ifstream inputFile("A1input.txt");
+    string inputString = "";
+    int inputCount = 0;
+    while (inputFile.is_open()) {
+        inputFile >> inputString;
+        inputCount++;
+        cout << inputCount;
+    }
     cout << "Hello World!\n";
 }
